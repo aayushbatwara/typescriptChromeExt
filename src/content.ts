19e7e649283aaeb5028394e016eff7ed0f57dcf7
Aayush <aayushbatwara@yahoo.com>
2024-05-01 13:01:10 +0800

    added content.ts and I get ECMA error

---
+++ src/content.ts
@@ -0,0 +1,4 @@
+//This script executes when user visits a website which is matched by sites listen in manifest.json's content script matches
+
+document.body.style.backgroundColor='#ffa2a2'
+console.log("Hello from content.ts")
